af6fe761b398c3c6d042e8bf90c543a228af02f3
Bilal Sahli <bilalsahli1999@gmail.com>
2022-04-10 15:26:58 +0200

    just another trial of Day4 challenge

--- DAY4-TRIAL2/_project_starter_/script.js
+++ DAY4-TRIAL2/_project_starter_/script.js
@@ -1,9 +1,8 @@
-const searchButton = document.querySelector('.btn')
-const searchContainer = document.querySelector('.search')
+const searchContainer = document.querySelector('.search-container')
 const input = document.querySelector('.input')
+const searchButton = document.querySelector('.btn')
 
 searchButton.addEventListener(('click'), () => {
     searchContainer.classList.toggle('active')
     input.focus()
-}
-)
+})
